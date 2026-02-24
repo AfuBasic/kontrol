3a87281ac0a417a1c9637ef1fa45ccb00d14539c
Afuwape Babatunde <afutunde@gmail.com>
2026-02-24 23:42:34 +0100

    Corrected email template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('assets/images/kontrol.png') }}" class="logo" alt="{{ config('app.name') }}" style="max-height: 50px; width: auto;">
+<img src="{{ asset('assets/images/kontrol.png') }}" class="logo" alt="{{ config('app.name') }}">
 </a>
 </td>
 </tr>
