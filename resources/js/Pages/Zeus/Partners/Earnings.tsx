da496cbe73492bc0b91f8e8ccdf71a76c25cb6b0
Afuwape Babatunde <afutunde@gmail.com>
2026-07-08 12:53:38 +0100

    style: remove inner container bg-color from Partners Earnings page

--- resources/js/Pages/Zeus/Partners/Earnings.tsx
+++ resources/js/Pages/Zeus/Partners/Earnings.tsx
@@ -67,7 +67,7 @@ export default function PartnerEarningsAdmin({ partner, earnings, summary }: Pro
         <ZeusLayout>
             <Head title={`${partner.name} – Earnings`} />
 
-            <div className="relative mx-auto max-w-7xl px-4 py-8 text-[#F2F3F6] bg-[#0A0B10] min-h-screen space-y-8">
+            <div className="relative mx-auto max-w-7xl px-4 py-8 text-[#F2F3F6] space-y-8">
                 {/* Decorative Glow */}
                 <div className="pointer-events-none absolute top-0 right-1/4 h-[500px] w-[500px] animate-pulse rounded-full bg-gradient-to-br from-[#6C5DFD]/5 to-[#A78BFA]/5 blur-[120px] duration-[8000ms]" />
 
