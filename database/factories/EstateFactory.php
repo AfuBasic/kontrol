b8ba5fdd7c644dd826ae3c09e8fa274c0e3b94ac
Afuwape Babatunde <afutunde@gmail.com>
2026-08-05 02:23:41 +0100

    feat(settings): add afterCreating hook to EstateFactory to seed default EstateSettings

--- database/factories/EstateFactory.php
+++ database/factories/EstateFactory.php
@@ -20,6 +20,40 @@ class EstateFactory extends Factory
         ];
     }
 
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Estate $estate) {
+            $estate->settings()->create([
+                'access_codes_enabled' => true,
+                'access_code_min_lifespan_minutes' => 15,
+                'access_code_max_lifespan_minutes' => 1440,
+                'access_code_single_use' => true,
+                'require_vehicle_information' => false,
+                'allow_residents_to_extend_visitor_passes' => true,
+                'visitor_checkout_enabled' => true,
+                'incident_categories' => [
+                    'Theft',
+                    'Noise Complaint',
+                    'Vandalism',
+                    'Unauthorized Entry',
+                    'Property Damage',
+                    'Medical Emergency',
+                ],
+                'default_incident_severity' => 'Low',
+                'require_photo_evidence_for_incidents' => false,
+                'require_resolution_notes_for_incidents' => false,
+                'allow_residents_to_report_incidents' => true,
+                'notify_admins_immediately_for_critical_incidents' => true,
+                'allow_partial_payments' => true,
+                'minimum_partial_payment_amount' => 100000,
+                'minimum_partial_payment_percentage' => 10,
+                'collection_reminder_frequency' => 'weekly',
+                'collection_maximum_reminder_attempts' => 3,
+                'send_reminder_before_due_date_days' => 1,
+            ]);
+        });
+    }
+
     public function active(): static
     {
         return $this->state(fn (array $attributes) => [
