b1e869b2f669d8ab56577dfc6d2d4726611ce151
Afuwape Babatunde <afutunde@gmail.com>
2026-08-11 00:48:23 +0100

    feat(model): create AdministrativeAssignment model

---
+++ app/Models/AdministrativeAssignment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
+
+class AdministrativeAssignment extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'is_primary' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function estate()
+    {
+        return $this->belongsTo(Estate::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function zone()
+    {
+        return $this->belongsTo(Zone::class);
+    }
+}
