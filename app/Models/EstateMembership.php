e71563930b10d7c6ca2f26906dea700799d30a17
Afuwape Babatunde <afutunde@gmail.com>
2026-08-11 00:48:23 +0100

    feat(model): create EstateMembership model

---
+++ app/Models/EstateMembership.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class EstateMembership extends Pivot
+{
+    protected $table = 'estate_users_membership';
+    
+    public $incrementing = true;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function estate()
+    {
+        return $this->belongsTo(Estate::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function zone()
+    {
+        return $this->belongsTo(Zone::class);
+    }
+
+    public function propertyOwner()
+    {
+        return $this->belongsTo(User::class, 'property_owner_id');
+    }
+}
