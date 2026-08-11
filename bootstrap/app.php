6c69607ae0c099eeb8e12e9229f85b11432fc8eb
Afuwape Babatunde <afutunde@gmail.com>
2026-08-11 01:10:58 +0100

    feat(auth): register ResolveContext middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -83,6 +83,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(
             append: [
                 AuthenticateSession::class,
+                \App\Http\Middleware\ResolveContext::class,
                 HandleInertiaRequests::class,
                 AddLinkHeadersForPreloadedAssets::class,
             ],
