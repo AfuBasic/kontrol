82846a67ed79d8117c2be651ab72dd13707a97d8
Afuwape Babatunde <afutunde@gmail.com>
2026-08-15 16:42:38 +0100

    policy: update ZonePolicy to check zones permissions falling back to admin role

--- app/Policies/ZonePolicy.php
+++ app/Policies/ZonePolicy.php
@@ -13,7 +13,7 @@ class ZonePolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->contextHasRole('admin');
+        return $user->contextHasRole('admin') || $user->hasPermissionTo('zones.view');
     }
 
     /**
@@ -27,7 +27,7 @@ class ZonePolicy
             return false;
         }
 
-        return $user->contextHasRole('admin');
+        return $user->contextHasRole('admin') || $user->hasPermissionTo('zones.view');
     }
 
     /**
@@ -35,7 +35,7 @@ class ZonePolicy
      */
     public function create(User $user): bool
     {
-        return $user->contextHasRole('admin');
+        return $user->contextHasRole('admin') || $user->hasPermissionTo('zones.create');
     }
 
     /**
@@ -49,7 +49,7 @@ class ZonePolicy
             return false;
         }
 
-        return $user->contextHasRole('admin');
+        return $user->contextHasRole('admin') || $user->hasPermissionTo('zones.edit');
     }
 
     /**
@@ -63,6 +63,6 @@ class ZonePolicy
             return false;
         }
 
-        return $user->contextHasRole('admin');
+        return $user->contextHasRole('admin') || $user->hasPermissionTo('zones.delete');
     }
 }
