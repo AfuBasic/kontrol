884e9b4d79d96dffbdebe3840b86aa14b8674662
Afuwape Babatunde <afutunde@gmail.com>
2026-08-17 11:39:18 +0100

    fix: revert to ref (PaystackPop.setup uses ref not reference)

--- resources/js/Pages/Web/Billing/PayCollection.tsx
+++ resources/js/Pages/Web/Billing/PayCollection.tsx
@@ -163,7 +163,7 @@ export default function PayCollection({
                 key: paystackKey,
                 email: validEmail,
                 amount: amountKobo,
-                reference: data.reference,
+                ref: data.reference,
                 channels: ['bank_transfer'],
                 callback_url: statusUrlFor(data.reference),
                 onClose: () => {
