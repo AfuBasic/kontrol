cc655973770545b7ab87036099b4867abcae73eb
Afuwape Babatunde <afutunde@gmail.com>
2026-08-21 02:02:00 +0100

    refactor(resident): prefix unused prop in Compliance Index

--- resources/js/Pages/Resident/Compliance/Index.tsx
+++ resources/js/Pages/Resident/Compliance/Index.tsx
@@ -52,7 +52,7 @@ interface Props {
     isCompliant: boolean;
 }
 
-export default function ResidentComplianceIndex({ violations, activeRestrictions, totalOutstanding, isCompliant }: Props) {
+export default function ResidentComplianceIndex({ violations, activeRestrictions, _totalOutstanding, isCompliant }: Props) {
     return (
         <div className="mx-auto max-w-4xl space-y-6 p-6">
             <Head title="Account Compliance & Standing" />
