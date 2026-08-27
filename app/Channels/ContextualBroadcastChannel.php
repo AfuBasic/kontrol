588cfff1d96c4ef1585f49c1f8483fb20089d364
Afuwape Babatunde <afutunde@gmail.com>
2026-08-27 11:38:48 +0100

    style: format ContextualBroadcastChannel with Pint

--- app/Channels/ContextualBroadcastChannel.php
+++ app/Channels/ContextualBroadcastChannel.php
@@ -11,11 +11,11 @@ class ContextualBroadcastChannel extends BroadcastChannel
     protected function getData($notifiable, Notification $notification)
     {
         $data = parent::getData($notifiable, $notification);
-        
+
         if (is_array($data)) {
             $contextService = app(NotificationContextService::class);
             $context = $contextService->attributesFor($notification, $notifiable, null);
-            
+
             $data = array_merge($data, $context);
         }
 
