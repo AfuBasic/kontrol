213a85df26e3e7c3a656aca5dbd1dc5b1a3932e2
Afuwape Babatunde <afutunde@gmail.com>
2026-08-28 14:29:08 +0100

    feat(zeus): pass active partners to estate details view

--- app/Http/Controllers/Zeus/EstateController.php
+++ app/Http/Controllers/Zeus/EstateController.php
@@ -185,6 +185,7 @@ class EstateController extends Controller
             'estate' => array_merge($estate->toArray(), [
                 'commission_days_remaining' => $estate->commissionDaysRemaining(),
             ]),
+            'partners' => Partner::active()->orderBy('name')->get(['id', 'name', 'email', 'commission_rate']),
             'residentStats' => $residentStats,
             'analytics' => [
                 'total_revenue' => $totalRevenue,
