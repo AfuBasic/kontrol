224ed16ddd8c6d516e0354ba56f19768ed779c5f
Afuwape Babatunde <afutunde@gmail.com>
2026-09-14 22:23:38 +0100

    feat(org-access): route /org/arrivals to Access hub with tab=arrivals

--- routes/organization.php
+++ routes/organization.php
@@ -37,7 +37,7 @@ Route::middleware(['auth', 'org.membership'])->prefix('org')->name('org.')->grou
 
     // Access: Arrivals & History
     Route::prefix('arrivals')->name('arrivals.')->group(function () {
-        Route::get('/', [ArrivalController::class, 'index'])->name('index');
+        Route::get('/', fn () => redirect()->route('org.access-list.index', ['tab' => 'arrivals']))->name('index');
         Route::get('/history', [ArrivalController::class, 'history'])->name('history');
         Route::post('/{log}/confirm', [ArrivalController::class, 'confirm'])->name('confirm');
     });
